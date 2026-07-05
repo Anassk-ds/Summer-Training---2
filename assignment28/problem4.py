00af7faa6721de58106d71c3b727635eac23499a
Anas Shaik <anasshaik382@gmail.com>
2026-07-05 21:21:07 +0530

    Improve smart server memory allocation code
    Refactor memory allocation logic for better readability.

--- assignment28/problem4.py
+++ assignment28/problem4.py
@@ -2,18 +2,14 @@
 n = int(input())
 arr = list(map(int, input().split()))
 limit = int(input())
-
 left = 0
 total = 0
 ans = 0
-
 for right in range(n):
     total += arr[right]
 
     while total > limit:
         total -= arr[left]
         left += 1
-
     ans = max(ans, right - left + 1)
-
 print(ans)
